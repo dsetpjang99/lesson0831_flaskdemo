03147b1b7c0abe06613dd489a84e49221013621b
danny <dsetpjang99@gmail.com>
2024-08-31 12:06:03 +0800

    新增index.html和books.html;使用迴圈方式輸出與使用css樣式設計

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)  # __name__ => __main__
 
-books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+# books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
 
 
 @app.route("/")
 def index():
     today = datetime.now()
+    name = "jerry"
     print(today)
-    return f"<h1>Hello Flask!<br>{today}</h1>"
+    # f"<h1>Hello Flask!<br>{today}</h1>"
+    return render_template("index.html", date=today, name=name)
 
 
 @app.route("/books")
 def show_books():
-    return books
+    # return books
+    print(books)
+
+    for key in books:
+        print(books[key])
+
+    return render_template("books.html", books=books)
 
 
 @app.route("/book/<int:id>")
